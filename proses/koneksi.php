d9e08e9e20cfdaf0191313f96e85c6124fcba505
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-17 10:50:02 +0700

    tambahan projectJanice

---
+++ proses/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+   $host = "localhost";
+   $user = "root";
+   $pass = "";
+   $db = "janice";	
+   $koneksi = mysqli_connect($host,$user,$pass,$db);
+
+?> 
---
+++ proses/prosesLogin.php
@@ -0,0 +1,26 @@
+<?php
+include("koneksi.php");
+SESSION_START();
+if(!$_POST['email'] || !$_POST['pass']){
+   // header("location:../login.php?alert=1");	
+   echo "kosong";
+}else {
+    $email  = $_POST['email'];
+    $pass = $_POST['pass'];
+    $sql  = "SELECT * FROM user WHERE email_u = '".$email."' AND password_u = '".$pass."'";
+    $cek=mysqli_query($koneksi,$sql);
+	$jumlah = mysqli_num_rows($cek);
+    $hasil = mysqli_fetch_assoc($cek);
+    if($jumlah == 0 ){
+        echo "gak ada";
+       // header("location:../login.php?alert=1");
+    }else {
+        $_SESSION['id_user'] 			= $hasil ['id_user'];
+        $_SESSION['id_roles']  	        = $hasil ['fk_id_al'];
+        $_SESSION['nama_u']	    	= $hasil['nama_u'];  
+        header("location:../index.php");
+        }
+    }
+
+
+?>
