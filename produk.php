2cb630190c3e2e369ab894d3732c37b0cd47c6bf
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-17 21:17:05 +0700

    update dari PEWE

--- produk.php
+++ produk.php
@@ -17,17 +17,36 @@
        <a href="tambahProduk.php"> <button style="float:right; background-color:#4295f5; color:white" class="btn btn-user">Tambah Data</button></a>
     </div>
   <div class="card-body">
+  <?php  
+      if (empty($_GET['alert'])) {
+        echo "";
+      } 
+  
+      elseif ($_GET['alert'] == 1) {
+        echo "<div class='alert alert-danger alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                <h4>  <i class='icon fa fa-times-circle'></i> Gagal Memasukan Data</h4>
+                Data yang anda masukan salah, silahkan di check kembali.
+              </div>";
+      }
+      elseif ($_GET['alert'] == 2) {
+        echo "<div class='alert alert-success alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                <h4>  <i class='icon fa fa-check-circle'></i> Success!</h4>
+                Anda telah berhasil mengupdate data.
+              </div>";
+	  }
+      ?>
     <div class="table-responsive">
       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead>
           <tr>
             <th>Nomor</th>
             <th>Nama Barang</th>
-            <th>Distributor</th>
-            <th>Harga Produk</th>
+            <th>Harga Distributor</th>
+            <th>Harga Reseller</th>
             <th>Desc Produk</th>
             <th>Gambar</th>
-            <th>Stok</th>
             <th>Status</th>
             <th>Action</th>
           </tr>
@@ -36,11 +55,10 @@
           <tr>
             <th>Nomor</th>
             <th>Nama Barang</th>
-            <th>Distributor</th>
-            <th>Harga Produk</th>
+            <th>Harga Distributor</th>
+            <th>Harga Reseller</th>
             <th>Desc Produk</th>
             <th>Gambar</th>
-            <th>Stok</th>
             <th>Status</th>
             <th>Action</th>
           </tr>
@@ -48,13 +66,11 @@
         <tbody>
         <?php
                   include("proses/koneksi.php");
-                  if($_SESSION['level'] == "1"){
-                    $query = "SELECT * FROM produk INNER JOIN user ON user.id_user = produk.fk_id_user LEFT JOIN stock on produk.id_produk = stock.fk_id_produk";
-                  } else if($_SESSION['level'] == "2"){
-                    $query = "SELECT * FROM produk INNER JOIN user ON user.id_user = produk.fk_id_user LEFT JOIN stock on produk.id_produk = stock.fk_id_produk where fk_id_user = '0'";
-                  }
+                  if($_SESSION['level'] == "1" || $_SESSION['level'] == "2"){
+                    $query = "SELECT * FROM produk INNER JOIN user ON user.id_user = produk.last_change ";
+                  } 
                   else if($_SESSION['level'] == "3"){
-                    $query = "SELECT * FROM produk INNER JOIN user ON user.id_user = produk.fk_id_user where fk_id_user = '".$_SESSION['level']."'";
+                    $query = "SELECT * FROM produk INNER JOIN user ON user.id_user = produk.last_change where last_change = '".$_SESSION['level']."'";
                   }
                   $nomor =1;
                   $tampilin = mysqli_query($koneksi, $query);
@@ -63,26 +79,22 @@
                     $id = $output['id_produk'];
                     $nama = $output['nama_produk'];
                     $user = $output['nama_u'];
-                    $id_user = $output['fk_id_user'];
+                    $id_user = $output['last_change'];
                     $harga = $output['harga_produk'];
+                    $reseller = $output['harga_reseller'];
                     $desc = $output['desc_produk'];
                     $gambar = $output['gambar_produk'];
                     $status = $output['status_produk'];
-                    if($id_user == "0"){
-                        $stok = "Unlimited";
-                    }else {
-                        $stok = $output['jumlah_stock'];
-                    }
+                   
                   
                   ?>
                     <tr>
                         <td><?php echo $nomor; ?></td>
-                        <td><?php echo $user; ?></td>
                         <td><?php echo $nama; ?></td>
                         <td><?php echo rupiah($harga); ?></td>
+                        <td><?php echo rupiah($reseller); ?></td>
                         <td><?php echo $desc; ?></td>
                         <td><image height ="100px" width ="100px" src="<?php echo $gambar ;?>"></td>
-                        <td><?php echo $stok; ?></td>
                         <td><?php 
                         if($status =="0"){
                             echo "Tidak Aktif"; 
@@ -97,7 +109,7 @@
                             data-deskripsi_barang='<?php echo $desc; ?>'
                             data-harga_barang='<?php echo $harga; ?>'
                             data-stock_barang='<?php echo $stok; ?>'
-                            data-status_barang='<?php echo $status; ?>'> edit</button> </td>
+                            data-status_barang='<?php echo $status; ?>'> edit</button>  </td>
                     </tr>
                     <?php
                     $nomor +=1;
@@ -121,39 +133,39 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-        <form id="form_send" action='proses_edit_barang.php' method ='post'>	
-        <input type='hidden' name='id_barang' id="id_barang">
-        <label for="exampleInputEmail1">Nama Barang</label>
-        <input type='text'class="form-control" name='nama_barang' id="nama_barang"> <br>
+          <form id="form_send" action='proses/prosesEditProduk.php' method ='post'  enctype="multipart/form-data">	
+          <input type='hidden' name='id_barang' id="id_barang">
+          <label for="exampleInputEmail1">Nama Produk</label>
+          <input type='text'class="form-control" name='nama_barang' id="nama_barang"> <br>
 
-        <label for="exampleInputEmail1">Deskripsi Barang</label> <br>
-        <input type='textarea' class="form-control" name='deskripsi_barang' id="deskripsi_barang"><br> 
+          <label for="exampleInputEmail1">Deskripsi Produk</label> <br>
+          <input type='textarea' class="form-control" name='deskripsi_barang' id="deskripsi_barang"><br> 
 
-        <label for="exampleInputEmail1">Harga Barang</label>
-        <input type='text' class="form-control" name='harga_barang' id="harga_barang"><br>
-        <label for="exampleInputEmail1">Stock Barang</label>
+          <label for="exampleInputEmail1">Harga Produk</label>
+          <input type='text' class="form-control" name='harga_barang' id="harga_barang"><br>
+          <label for="exampleInputEmail1">Stock Produk</label>
 
-        <input type='text' class="form-control" name='stock_barang' id="stock_barang"><br>
+          <input type='text' class="form-control" name='stock_barang' id="stock_barang"><br>
+          <label for="exampleInputEmail1">Gambar Produk</label> <br>
+          <input type="file" name="berkas" id="berkas"> <br> <br>    
+          <label for="exampleInputEmail1">Status</label>
+          <Select class="form-control" name='status_barang'>
+          <option value='1'> Aktif </option>
+          <option value='0'> Tidak Aktif</option> 
+          </select><br>
+          <input type='submit' class="btn btn-primary" value='submit'>
 
-        <label for="exampleInputEmail1">Status</label>
-        <Select class="form-control" name='status_barang'>
-        <option value='1'> Aktif </option>
-        <option value='0'> Tidak Aktif</option> 
-        </select><br>
-        <input type='submit' class="btn btn-primary" value='submit'>
 
-
-        </form>
+          </form>
         </div>
-     
       </div>
     </div>
   </div>
-</div>
 
 
-     <!-- Footer -->
-     <footer class="sticky-footer bg-white">
+
+   <!-- Footer -->
+   <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>Copyright &copy; <?php echo "DAk Dak";?></span>
@@ -191,7 +203,7 @@
       </div>
     </div>
   </div>
-                 
+
   <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -212,41 +224,42 @@
   <script src="js/demo/chart-pie-demo.js"></script>
   <script src="js/demo/datatables-demo.js"></script>
   <script>
-  $(document).ready(function() {
-    $(".btn_edit").click(function(event){
-			var id_barang = $(this).data('id_barang');
-			var nama_barang = $(this).data('nama_barang');
-			var deskripsi_barang = $(this).data('deskripsi_barang');
-			var harga_barang = $(this).data('harga_barang');
-			var stock_barang = $(this).data('stock_barang');
-			var status_barang = $(this).data('status_barang');
-      
-		
-			$("#id_barang").val(id_barang);
-			$("#nama_barang").val(nama_barang);
-			$("#deskripsi_barang").val(deskripsi_barang);
-			$("#harga_barang").val(harga_barang);
-      if(stock_barang == "Unlimited"){
-        $("#stock_barang").val(stock_barang);
-        document.getElementById("stock_barang").disabled = true;
-      }else {
-        $("#stock_barang").val(stock_barang);
-        document.getElementById("stock_barang").disabled = false;
-      }
-			
-			$("#status_barang").val(status);
-			
-			
-			/*
-			$('#form_send').form('clear');
-			$("#myModal").modal({
-				backdrop: "static"
-			});
-			*/
-			
-		});
+		 $(document).ready(function() {
+        $(".btn_edit").click(function(event){
+          var id_barang = $(this).data('id_barang');
+          var nama_barang = $(this).data('nama_barang');
+          var deskripsi_barang = $(this).data('deskripsi_barang');
+          var harga_barang = $(this).data('harga_barang');
+          var stock_barang = $(this).data('stock_barang');
+          var status_barang = $(this).data('status_barang');
+          
+        
+          $("#id_barang").val(id_barang);
+          $("#nama_barang").val(nama_barang);
+          $("#deskripsi_barang").val(deskripsi_barang);
+          $("#harga_barang").val(harga_barang);
+          if(stock_barang == "Unlimited"){
+            $("#stock_barang").val(stock_barang);
+            document.getElementById("stock_barang").readOnly  = true;
+          }else {
+            $("#stock_barang").val(stock_barang);
+            document.getElementById("stock_barang").readOnly  = false;
+          }
+          
+          $("#status_barang").val(status);
+          
+          
+          /*
+          $('#form_send').form('clear');
+          $("#myModal").modal({
+            backdrop: "static"
+          });
+          */
+          
+        });
   });
-</script>
+  </script>
+  
 </body>
 
 </html>
