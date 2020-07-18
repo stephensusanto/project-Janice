be64f22a20a6b04cd7fe12b6bbfb61b9036a202f
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-18 13:09:42 +0700

    Update konfig.php

--- konfig.php
+++ konfig.php
@@ -8,7 +8,7 @@
        <div class="container-fluid">
 
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Data Akses Level</h1>
+<h1 class="h3 mb-2 text-gray-800">Data Konfigurasi</h1>
 
 
 <!-- DataTales Example -->
