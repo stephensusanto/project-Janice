70ad4baf17c4daa24b046a2dc83e77cbc0789aaf
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-23 12:06:14 +0700

    penambahan header

---
+++ distributor/header.php
@@ -0,0 +1,29 @@
+<div id="mainMenu-trigger">
+                        <a class="lines-button x"><span class="lines"></span></a>
+                    </div>
+                    <!--end: Navigation Resposnive Trigger-->
+                    <!--Navigation-->
+                    <div id="mainMenu">
+                        <div class="container">
+                            <nav>
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="index.html">Pesan Stock</a></li>
+                                    <li><a href="index.html">Konfirmasi Pembayaran</a></li>
+                                    
+                                    <li class="dropdown"><a href="#">Lihat Laporan</a>
+                                        <ul class="dropdown-menu">
+                                            
+                                            <li><a href="#">Laporan Pemesanan Reseller</a></li>
+                                             <li><a href="#">Laporan Stock Barang</a></li>
+                                            
+                                        </ul>
+                                    </li>
+                                    
+                                    <li><a href="index.html">Profil Saya</a></li>
+                                     <li><a href="proses/prosesLogout.php">Log Out</a></li>
+                                    
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
