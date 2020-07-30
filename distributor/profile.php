b5a6dba1cf170fe33da0b5d1c7b9cac764818be3
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-30 14:19:41 +0700

    Create profile.php

---
+++ distributor/profile.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<?php 
+    SESSION_START();
+    include ("proses/koneksi.php");
+    if(!$_SESSION['nama_u']){
+        header("location:login.php");
+    }
+
+?>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="INSPIRO" />
+    <meta name="description" content="Themeforest Template Polo">
+    <!-- Document title -->
+    <title>Home </title>
+    <!-- DataTables css -->
+    <link href='plugins/datatables/datatables.min.css' rel='stylesheet' />
+    <!-- Stylesheets & Fonts -->
+    <link href="css/plugins.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <header id="header" data-fullwidth="true">
+            <div class="header-inner">
+                <div class="container">
+                    <!--Logo-->
+                    
+                    <div id="logo"> <a href="index.html"><span class="logo-default">DAKDAK SALTED EGG</span><span class="logo-dark">POLO</span></a> </div>
+                    <!--End: Logo-->
+                    <!-- Search -->
+                    <div id="search"><a id="btn-search-close" class="btn-search-close" aria-label="Close search form"><i class="icon-x"></i></a>
+                        <form class="search-form" action="search-results-page.html" method="get">
+                            <input class="form-control" name="q" type="search" placeholder="Type & Search..." />
+                            <span class="text-muted">Start typing & press "Enter" or "ESC" to close</span>
+                        </form>
+                    </div>
+                    <!-- end: search -->
+                    <!--Header Extras
+                    <div class="header-extras">
+                        <ul>
+                            <li>
+                                <a id="btn-search" href="#"> <i class="icon-search"></i></a>
+                            </li>
+                            <li>
+                                <div class="p-dropdown">
+                                    <a href="#"><i class="icon-globe"></i><span>EN</span></a>
+                                    <ul class="p-dropdown-content">
+                                        <li><a href="#">French</a></li>
+                                        <li><a href="#">Spanish</a></li>
+                                        <li><a href="#">English</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--end: Header Extras-->
+                    <!--Navigation Resposnive Trigger-->
+                    <?php 
+                        include("header.php");
+                    ?>
+                    <!--end: Navigation-->
+                </div>
+            </div>
+        </header>
+        <!-- end: Header -->
+        <!-- Page title -->
+        <section id="page-title" data-bg-parallax="images/bg2.jpg" >
+            <div class="container">
+                <div class="page-title">
+                    <h1>Selamat Datang Distributor <?php echo $_SESSION['nama_u']; ?></h1>
+                    <span>Inspiration comes of working every day.</span>
+                </div>
+                <div class="breadcrumb">
+                    <ul>
+                        <li><a href="index.php">Home</a> </li>
+                        <li><a href="profile.php">Profile</a> </li>  
+                        
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- end: Page title -->
+        <!-- Page Menu -->
+      <!-- end: Page Menu -->
+        <!-- Page Content -->
+        <section id="page-content" class="no-sidebar">
+            <div class="container">
+                <!-- DataTable -->
+                
+                        <h1>Profile Anda</h1>
+                        
+                                <button class="btn btn-lg btn-slide " data-width="240" href="#">
+                                    <i class="fa fa-cog"></i>
+                                    <span>Ganti Profile</span>
+                                </button>
+                
+                     
+                                <button class="btn btn-lg btn-slide btn-dark" data-width="240" href="#">
+                                    <i class="fa fa-lock"></i>
+                                    <span>Ganti Password</span>
+                                </button>
+                           
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputEmail4">Nama</label>
+                                    <input class="form-control" value="BEBEKK " id="inputEmail4" placeholder="Nama" type="text">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Email</label>
+                                    <input class="form-control" id="inputPassword4" placeholder="Email" type="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress">Alamat</label>
+                               
+                                <textarea class="form-control" id="inputAddress" placeholder="asdasdasdasa" value="asdasdasdsa" rows="3"></textarea>
+                                
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress2">Address 2</label>
+                                <input class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" type="text">
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputCity">City</label>
+                                    <input class="form-control" id="inputCity" type="text">
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputState">State</label>
+                                    <select id="inputState" class="form-control">
+                                        <option selected="">Choose...</option>
+                                        <option>...</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <label for="inputZip">Zip</label>
+                                    <input class="form-control" id="inputZip" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="form-check">
+                                    <input class="form-check-input" id="gridCheck" type="checkbox">
+                                    <label class="form-check-label" for="gridCheck">
+                                        Check me out
+                                    </label>
+                                </div>
+                            </div>
+                            
+                        </form>
+                <!-- end: DataTable -->
+            </div>
+        </section>
+        <!-- end: Page Content -->
+        <!-- Footer -->
+        <?php 
+                        include("footer.php");
+                    ?>
+        <!-- end: Footer -->
+    </div> <!-- end: Body Inner -->
+    <!-- Scroll top -->
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+    <!--Popover plugin files-->
+    <script src="plugins/popper/popper.min.js"></script>
+
+    <!--Plugins-->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <!--Template functions-->
+    <script src="js/functions.js"></script>
+
+    <!--Datatables plugin files-->
+    <script src='plugins/datatables/datatables.min.js'></script>
+    <script>
+        $(document).ready(function () {
+            var table = $('#datatable').DataTable({
+                buttons: [{
+                    extend: 'print',
+                    title: 'Test Data export',
+                    exportOptions: {
+                        columns: "thead th:not(.noExport)"
+                    }
+                }, {
+                    extend: 'pdf',
+                    title: 'Test Data export',
+                    exportOptions: {
+                        columns: "thead th:not(.noExport)"
+                    }
+                }, {
+                    extend: 'excel',
+                    title: 'Test Data export',
+                    exportOptions: {
+                        columns: "thead th:not(.noExport)"
+                    }
+                }, {
+                    extend: 'csv',
+                    title: 'Test Data export',
+                    exportOptions: {
+                        columns: "thead th:not(.noExport)"
+                    }
+                }, {
+                    extend: 'copy',
+                    title: 'Test Data export',
+                    exportOptions: {
+                        columns: "thead th:not(.noExport)"
+                    }
+                }]
+            });
+            table.buttons().container().appendTo('#export_buttons');
+            $("#export_buttons .btn").removeClass('btn-secondary').addClass('btn-light');
+        });
+    </script>
+</body>
+
+</html>
