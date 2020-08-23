2c463303a3ed6ca7db41db7d240689542f28135d
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-08-23 17:30:51 +0700

    bug fix

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
           <div class="row">
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -58,7 +58,7 @@
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -96,47 +96,9 @@
               </div>
             </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Produk yang Tersedia </div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                        <?php
-                        
-                        $tanggal = date("Y/m/d");
-                          $sql  = "SELECT count(*) as itung FROM Produk Where status_produk = '1'";
-                          $cek=mysqli_query($koneksi,$sql);
-                          $jumlah = mysqli_num_rows($cek);
-                          if($jumlah == 0){
-                            $a = 0;
-                          }else {
-                            $ambil = mysqli_fetch_assoc($cek);
-                            $a = $ambil['itung'];
-                          }
-                        
-
-                      ?>
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $a; ?></div>
-                        </div>
-                        <div class="col">
-                         
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
             <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
